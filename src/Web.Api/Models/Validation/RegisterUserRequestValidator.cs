aea30d91f4599e6e73cd6ca8930ed86dba4be6e5
Chakir Belhaj <c.belhaj_zakelijk@hotmail.com>
2019-03-12 18:17:04 +0100

    starting authentication project

---
+++ src/Web.Api/Models/Validation/RegisterUserRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Web.Api.Models.Request;
+
+namespace Web.Api.Models.Validation
+{
+    public class RegisterUserRequestValidator : AbstractValidator<RegisterUserRequest>
+    {
+        public RegisterUserRequestValidator()
+        {
+            RuleFor(x => x.FirstName).Length(2, 30);
+            RuleFor(x => x.LastName).Length(2, 30);
+            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Password).Length(6, 15);
+        }
+    }
+}
